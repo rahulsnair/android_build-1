944bf360ece14f424af64ba8d56de2bd71064a31
Andrew Dodd <atd7@cornell.edu>
2013-09-28 15:26:50 -0400

    Set XOS_BUILD environment variable
    This is used to gate a few features, including backuptool
    
    Change-Id: I02fee9e0765cca88c833bb0514762aa3b14642e3
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- envsetup.sh
+++ envsetup.sh
@@ -166,6 +166,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^XOS_") ; then
+       XOS_BUILD=$(echo -n $1 | sed -e 's/^XOS_//g')
+    else
+       XOS_BUILD=
+    fi
+    export XOS_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
