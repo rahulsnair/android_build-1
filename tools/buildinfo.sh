b98c10b925ce7c3aa64c1d989d3dd543569f7f29
xdevs23 <xdevs23@outlook.com>
2016-04-20 20:16:45 +0200

    buildinfo.sh: add xos version property

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -20,6 +20,9 @@ echo "ro.build.user=$USER"
 echo "ro.build.host=`hostname`"
 echo "ro.build.tags=$BUILD_VERSION_TAGS"
 echo "ro.build.flavor=$TARGET_BUILD_FLAVOR"
+# This needs to be defined somewhere
+echo "ro.xos.version=$BUILD_HALOGEN_VERSION"
+
 echo "ro.product.model=$PRODUCT_MODEL"
 echo "ro.product.brand=$PRODUCT_BRAND"
 echo "ro.product.name=$PRODUCT_NAME"
