f3ea12a07709af2009d9ded296a1b3357b887f57
Neil Agarwal <regalstreak@gmail.com>
2016-04-26 16:54:51 +0000

    core/config.mk: Use halogenOS vendor repo

--- core/config.mk
+++ core/config.mk
@@ -692,6 +692,6 @@ include $(BUILD_SYSTEM)/qcom_target.mk
 
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/aosp/sepolicy/sepolicy.mk)
+$(eval include vendor/xos/sepolicy/sepolicy.mk)
 
 include $(BUILD_SYSTEM)/dumpvar.mk
