ba24312170bbaffa62b55a8722d27017b64f9d50
Neil Agarwal <regalstreak@gmail.com>
2016-07-02 13:49:45 +0200

    product: Strip my babe down

--- target/product/core.mk
+++ target/product/core.mk
@@ -23,7 +23,6 @@
 PRODUCT_PACKAGES += \
     BasicDreams \
     Browser \
-    Calendar \
     CalendarProvider \
     CaptivePortalLogin \
     CertInstaller \
@@ -31,9 +30,7 @@ PRODUCT_PACKAGES += \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
     ExactCalculator \
-    Exchange2 \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
