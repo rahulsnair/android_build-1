DirtyUnicorns/android_build

Change path to definitions in vendor
Pending merge of http://gerrit.dirtyunicorns.com/#/c/28039/

Change-Id: Icce65d75dd1b5374dd3172d9c1b76a45408234b8

core/definitions.mk
@@ -2542,7 +2542,7 @@ endef
 include $(BUILD_SYSTEM)/distdir.mk
 
 # Include any vendor specific definitions.mk file
--include $(TOPDIR)vendor/*/build/core/definitions.mk
+-include $(TOPDIR)vendor/du/config/definitions.mk
 -include $(TOPDIR)device/*/build/core/definitions.mk
 
 # broken:

rahulsnair/android_build-1

core/definitions.mk
@@ -2923,7 +2923,7 @@ endef
 include $(BUILD_SYSTEM)/distdir.mk
 
 # Include any vendor specific definitions.mk file
--include $(TOPDIR)vendor/*/build/core/definitions.mk
+-include $(TOPDIR)vendor/xos/config/definitions.mk
 -include $(TOPDIR)device/*/build/core/definitions.mk
 -include $(TOPDIR)product/*/build/core/definitions.mk
 