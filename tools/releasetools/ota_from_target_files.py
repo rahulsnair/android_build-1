4f72a352107b3bf8e24c3f4db9c7fd62c38bba7c
Hunter <hbashton13@gmail.com>
2016-10-12 00:59:21 -0500

    fix backup tool attempt
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -780,6 +780,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   if OPTIONS.backuptool:
     script.ShowProgress(0.02, 10)
+    script.Mount("/system")
     script.RunBackup("restore")
 
   if block_based:
